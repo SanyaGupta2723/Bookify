045fa3f32ba42ca61b19070eed777cfa046f705f
Sanya Gupta <sanyakvds@gmail.com>
2026-01-05 22:32:22 +0530

    main page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import PopularBook from "./components/PopularBook";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-base-100">
       <NavBar />
+     
 
-      {/* PAGE CONTENT */}
+      
       <main className="flex-grow">
         <Hero />
+        <PopularBook />
       </main>
-
-      {/* LIGHT SEPARATOR */}
       <div className="w-full h-px bg-white/10"></div>
-
-      {/* FOOTER (NORMAL FLOW) */}
       <Footer />
+      
     </div>
   );
 }
--- Frontend/src/components/PopularBook.jsx
+++ Frontend/src/components/PopularBook.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import list from '../../public/list.json';
 
 function PopularBook() {
-    const filterData = list.filter((data) => data.category === "Free"); 
-    console.log(filterData);
   return (
-    <div>
-      
+    <div className="max-w-7xl mx-auto px-0 mt-10">
+      <h1 className="text-xl font-semibold text-white">
+        Popular Books
+      </h1>
     </div>
   )
 }
