3ef1e94d73a845e450212ea8a23656edf95733a5
Sanya Gupta <sanyakvds@gmail.com>
2026-01-09 21:46:32 +0530

    login button

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -3,12 +3,10 @@ import React from "react";
 function Login() {
   return (
     <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-      <div className="modal-box max-w-md">
+      <div className="modal-box max-w-md mb-8">
 
-        <h3 className="font-bold text-lg mb-1">Login</h3>
-        <p className="text-sm opacity-70 mb-5">
-          Press ESC key or click the button below to close
-        </p>
+        <h3 className="font-bold text-lg mb-8">Login</h3>
+        
 
         {/* ✅ FORM FIELD */}
         <div className="form-control w-full mb-8">
@@ -21,14 +19,32 @@ function Login() {
     className="input input-bordered w-full"
   />
 </div>
-
+<div className="form-control w-full mb-8">
+  <label className="label mb-4">
+    <span className="label-text text-sm">Password</span>
+  </label>
+  <input
+    type="password"
+    placeholder="Enter your password"
+    className="input input-bordered w-full"
+  />
+</div>
 
         {/* ✅ ACTION BUTTONS */}
-        <div className="modal-action mt-2">
+        <div className="modal-action mt-5 gap-6">
           <form method="dialog">
             <button className="btn btn-outline">Cancel</button>
           </form>
-          <button className="btn btn-primary">Login</button>
+          <button
+  className="
+  btn
+  bg-transparent
+  border-2 border-primary
+
+  transition"
+>
+  Login
+</button>
         </div>
 
       </div>
