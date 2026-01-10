eb5d285349cc6dace090595cced1d8271567e4b3
Sanya Gupta <sanyakvds@gmail.com>
2026-01-10 22:25:39 +0530

    signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Fiction" element={<Fiction />} />
+      <Route path="/signup" element={<Signup />} />
       
     </Routes>
   )
