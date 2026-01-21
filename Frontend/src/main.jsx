21ca0c14add53414129df28b9d5c8225d7fccc7c
Sanya Gupta <sanyakvds@gmail.com>
2026-01-21 21:45:31 +0530

    fr

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
+import "./index.css"; // ⭐ MUST
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
