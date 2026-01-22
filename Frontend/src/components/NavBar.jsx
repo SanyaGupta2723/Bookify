4a36b4257ee81638dcebd9b7b8aeb2e2d78d9493
Sanya Gupta <sanyakvds@gmail.com>
2026-01-22 19:58:11 +0530

    nb

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -82,7 +82,7 @@ useEffect(() => {
         {/* CENTER */}
         <div className="hidden lg:flex flex-1 justify-center">
           <ul className="menu menu-horizontal gap-6">
-            <li><Link to="/" className={menuItemClass}>Home</Link></li>
+            <li><Link to="/" className={`${menuItemClass} hover-neon px-4 py-2`}>Home</Link></li>
             <li><Link to="/Fiction" className={menuItemClass}>Fiction</Link></li>
             <li><Link to="/Nonfiction" className={menuItemClass}>Non-Fiction</Link></li>
             <li><Link to="/Contact" className={menuItemClass}>Contact</Link></li>
