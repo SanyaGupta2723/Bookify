29634e6381e2922cc4c640d7f0c7829b872c21dc
Sanya Gupta <sanyakvds@gmail.com>
2026-01-23 11:28:31 +0530

    navbar

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -63,7 +63,8 @@ useEffect(() => {
     "relative px-3 py-2 font-medium text-base-content transition-colors duration-300 hover:text-primary after:absolute after:left-1/2 after:-bottom-1 after:h-[3px] after:w-0 after:bg-primary after:rounded-full after:transition-all after:duration-300 hover:after:w-full hover:after:left-0";
 
   return (
-    <nav className="sticky top-0 z-50 bg-base-100 px-6 border-b-2 border-primary/30 shadow-[0_4px_20px_rgba(99,102,241,0.15)]">
+    <nav className="relative sticky top-0 z-50 bg-base-100 px-6 border-b-2 border-primary/30 shadow-[0_4px_20px_rgba(99,102,241,0.15)]">
+
       <div className="flex items-center h-16">
 
         {/* LEFT */}
@@ -75,7 +76,7 @@ useEffect(() => {
             ☰
           </button>
           <span className="text-xl font-bold whitespace-nowrap">
-            Bookify 📚
+            KitaBify 📚
           </span>
         </div>
 
