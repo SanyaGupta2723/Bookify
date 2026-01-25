b6fb9ec4830a4e05b14236edf7cf776cb1354e7f
Sanya Gupta <sanyakvds@gmail.com>
2026-01-25 13:47:11 +0530

    pay

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 
 
