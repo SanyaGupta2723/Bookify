860deb9cec1bc89b209af00a08ef637949e4ad2d
Sanya Gupta <sanyakvds@gmail.com>
2026-02-02 20:27:28 +0530

    db

---
+++ Backend/utils/sendMail.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+const sendMail = async (name, email, message) => {
+  // 1️⃣ transporter banao (email bhejne ka engine)
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,      // tumhara gmail
+      pass: process.env.EMAIL_PASS // gmail app password
+    },
+  });
+
+  // 2️⃣ actual mail bhejo
+  await transporter.sendMail({
+    from: email,                  // user ka email
+    to: process.env.EMAIL,        // tumhara email
+    subject: "New Contact Message 📩",
+    html: `
+      <h2>New Contact Form Message</h2>
+      <p><b>Name:</b> ${name}</p>
+      <p><b>Email:</b> ${email}</p>
+      <p><b>Message:</b></p>
+      <p>${message}</p>
+    `,
+  });
+};
+
+export default sendMail;
