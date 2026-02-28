1bc9429117fac33bc10685b03d10809e3a111b29
Sanya Gupta <sanyakvds@gmail.com>
2026-02-28 22:31:25 +0530

    md

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,8 +1,8 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const sendMail = async (name, email, message) => {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: process.env.EMAIL,
