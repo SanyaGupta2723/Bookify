a24b0e3c2b6d7fd1816ed9e539d6ddbc0639ea1e
Sanya Gupta <sanyakvds@gmail.com>
2026-02-28 23:17:15 +0530

    ,

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,27 +1,21 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
 const sendMail = async (name, email, message) => {
-  
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
-  await transporter.sendMail({
-    from: process.env.EMAIL,
-    to: process.env.EMAIL, // 👈 tumhari personal mail yaha jayegi
-    subject: "New Contact Message - Kitabify",
+  const response = await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: process.env.EMAIL,
+    subject: "New Contact Message",
     html: `
-      <h3>New Contact Message</h3>
+      <h3>New Message</h3>
       <p><b>Name:</b> ${name}</p>
       <p><b>Email:</b> ${email}</p>
       <p><b>Message:</b> ${message}</p>
     `,
   });
+
+  console.log(response);
 };
-console.log("PASS LENGTH:", process.env.EMAIL_PASS?.length);
 
 export default sendMail;
